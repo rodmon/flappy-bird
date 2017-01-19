ca2a4be9a9fc1173dd0aa92554afcbb1e8981057
Rodrigo <rodrigo.monte86@gmail.com>
2017-01-18 22:03:19 -0500

    Placing the canvas where I want it to be

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // Initialize Phaser, and create a 400px by 490px game
-var game = new Phaser.Game(400, 490);
+var game = new Phaser.Game(400, 490, Phaser.AUTO, 'game-div', { });
 
 // Create our 'main' state that will contain the game
 var mainState = {
